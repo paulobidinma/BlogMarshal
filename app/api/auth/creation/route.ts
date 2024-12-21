08e502fcc116639bbc55ede1776271cae7aec84c
paulobidinma <oziomaobidinma3@gmail.com>
2024-12-21 04:10:49 +0100

    reroute ready

--- app/api/auth/creation/route.ts
+++ app/api/auth/creation/route.ts
@@ -31,7 +31,7 @@ export async function GET() {
 
   return NextResponse.redirect(
     process.env.NODE_ENV === "production"
-      ? "https://blog-marshal.vercel.app/dasboard"
+      ? "https://blog-marshal-coral.vercel.app/dasboard"
       : "http://localhost:3000/dashboard"
   );
 }
